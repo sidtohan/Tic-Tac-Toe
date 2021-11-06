1bd9159238fc34c1e10ef81fa91217416ed54cb8
Siddhant Tohan <siddhant.tohan@gmail.com>
2021-11-06 10:34:15 +0530

    Added the click listeners successfully, as well as the logic that checks if it is filled

--- script.js
+++ script.js
@@ -1,47 +1,69 @@
 // Global Objects
 const GameBoard = (function () {
   // using 2D array for simplicity
-  const arr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  const _gameBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  let _currentTurn = 0;
+  // 0 means player, and 1 means PC
 
-  const checkWin = () => {
+  // Methods
+  const _checkWin = () => {
     // some code
   }
 
-  const updateBlock = (e) => {
+  const _placeMarker = (e) => {
     // some code
-  }
-
-  const addListeners = (e) => {
-    // call from generate board
-  }
-
-  const clear = () => {
-    // clear array and call generateBoard
-  }
-
-  const generateBoard = () => {
-    for(let i = 0; i < 3; i++){
-      for(let j = 0; j < 3; j++){
-        let block = document.createElement('div');
-        block.classList.add('board-block');
-        block.setAttribute('data-row',i);
-        block.setAttribute('data-column',j);
-        gameBoardDiv.appendChild(block);
+    let row = e.target.getAttribute('data-row');
+    let column = e.target.getAttribute('data-column');
+    if(_checkValidBlock(row,column)){
+      let block = document.querySelector(`div[data-row="${row}"][data-column="${column}"`);
+      block.textContent = (_currentTurn === 0? user.getMarker() : pc.getMarker());
+      _gameBoard[row][column] = (_currentTurn === 0? user.getMarker() : pc.getMarker());
+      if(_currentTurn === 0){
+        _currentTurn = 1;
+      } else{
+        _currentTurn = 0;
       }
     }
   }
 
-  const checkValidBlock = () => {
-    // checks if space clicked was empty
+  const _addListeners = (e) => {
+    // call from generate board
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let block = document.querySelector(`div[data-row="${i}"][data-column="${j}"]`);
+        block.addEventListener('click', _placeMarker);
+      }
+    }
+  }
+
+  const _clear = () => {
+    // clear array and call generateBoard
+  }
+
+  const generateBoard = () => {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let block = document.createElement('div');
+        block.classList.add('board-block');
+        block.setAttribute('data-row', i);
+        block.setAttribute('data-column', j);
+        gameBoardDiv.appendChild(block);
+      }
+    }
+    _addListeners();
+  }
+
+  const _checkValidBlock = (row, column) => {
+    return _gameBoard[row][column] === 0;
+    // 0 means it was empty
   }
 
   return {
-    generateBoard,
-    updateBlock
+    generateBoard
   }
 })();
 
-const Player = function (chosenName,chosenMarker) {
+const Player = function (chosenName, chosenMarker) {
   let score = 0;
   let marker = chosenMarker;
   let name = chosenName;
@@ -49,7 +71,7 @@ const Player = function (chosenName,chosenMarker) {
   const getName = () => {
     return name;
   }
-  
+
   const getMarker = () => {
     return marker;
   }
